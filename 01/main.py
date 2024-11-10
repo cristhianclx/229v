c05d48a2d2cdc9cda0db8fa719a1f62645d40b99
Cristhian <cristhianclx@gmail.com>
2024-11-10 10:55:57 -0500

    01

--- 01/main.py
+++ 01/main.py
@@ -1,7 +1,57 @@
-from flask import Flask
+from flask import Flask, request
 
 app = Flask(__name__)
 
 @app.route("/")
 def hello_world():
     return "<p>Hello, World!</p>"
+
+@app.route("/data")
+def data():
+    return "<p>DATA :)<p>"
+
+@app.route("/students")
+def students():
+    return [{
+        "id": 123,
+        "name": "abc"
+    }, {
+        "id": 456,
+        "name": "def"
+    }]
+
+@app.route("/students/<int:id>")
+def students_by_id(id):
+    return {
+        "id": id,
+        "name": "abc"
+    }
+
+
+data = [{
+    "user": "frank",
+    "password": "123456",
+}, {
+    "user": "cristhian",
+    "password": "123123",
+}, {
+    "user": "raul",
+    "password": "12345678",
+}]
+
+
+@app.route("/login", methods=["GET", "POST"])
+def login():
+    if request.method == "GET":
+        return "you need to login"
+    if request.method == "POST":
+        raw = request.get_json()
+        user = raw.get("user") # raw["user"]
+        password = raw.get("password")
+        if user and password:
+            print(user)
+            print(password)
+            # es valido => "valid login"
+            return "invalid login"
+        else:
+            return "missing data"
